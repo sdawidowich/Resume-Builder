4cfb433600562e54cd67e4bf52f794bba9a78035
sdawidowich <79337885+sdawidowich@users.noreply.github.com>
2024-02-03 15:37:32 -0600

    Added fragment element so a single element is returned.

--- src/components/ResumeOutput/ResumeOutput.jsx
+++ src/components/ResumeOutput/ResumeOutput.jsx
@@ -4,43 +4,48 @@ import OutputSectionDetails from './OutputSectionDetails/OutputSectionDetails';
 
 function ResumeOutput({personalDetails, educationDetails, workExpDetails}) {
     return(
-        <>
+        <div className='resume-output'>
             <OutputPersonalDetails personalDetails={personalDetails} />
             <OutputSectionDetails heading="Education">
+                <>
                 {
-                educationDetails.map((item) => {
-                    return (
-                        <div key={item.id + "-output"}>
-                            <h4 className='heading'>{item.School}</h4>
-                            <div className="details output">
-                                <div className='degree output'>{item.Degree}</div>
-                                <div className='startDate output'>{item.StartDate}</div>
-                                <div className='endDate output'>{item.EndDate}</div>
-                                <div className='gpa output'>{item.GPA}</div>
+                    educationDetails.map((item) => {
+                        return (
+                            <div key={item.id + "-output"}>
+                                <h4 className='heading'>{item.School}</h4>
+                                <div className="details output">
+                                    <div className='degree output'>{item.Degree}</div>
+                                    <div className='startDate output'>{item.StartDate}</div>
+                                    <div className='endDate output'>{item.EndDate}</div>
+                                    <div className='gpa output'>{item.GPA}</div>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })
+                        )
+                    })
                 }
+                </>
             </OutputSectionDetails>
             <OutputSectionDetails heading="Work Experience">
+                <>
                 {
-                workExpDetails.map((item) => {
-                    return (
-                        <div key={item.id + "-output"}>
-                            <h4 className='heading'>{item.Company}</h4>
-                            <div className="details output">
-                                <div className='degree output'>{item.Position}</div>
-                                <div className='startDate output'>{item.StartDate}</div>
-                                <div className='endDate output'>{item.EndDate}</div>
-                                <div className='gpa output'>{item.Description}</div>
+                    workExpDetails.map((item) => {
+                        return (
+                            <div key={item.id + "-output"}>
+                                <h4 className='heading'>{item.Company}</h4>
+                                <div className="details output">
+                                    <div className='degree output'>{item.Position}</div>
+                                    <div className='startDate output'>{item.StartDate}</div>
+                                    <div className='endDate output'>{item.EndDate}</div>
+                                    <div className='gpa output'>{item.Description}</div>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })
+                        )
+                    })
                 }
+                </>
+                
             </OutputSectionDetails>
-        </>
+        </div>
     );
 }
 
