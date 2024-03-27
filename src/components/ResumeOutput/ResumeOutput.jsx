9898a6f9be586c5c196d8eec9809c581a96c1746
sdawidowich <79337885+sdawidowich@users.noreply.github.com>
2024-03-27 17:15:48 -0500

    Somewhat responsive resume display for mobile.

--- src/components/ResumeOutput/ResumeOutput.jsx
+++ src/components/ResumeOutput/ResumeOutput.jsx
@@ -1,11 +1,31 @@
-import PropTypes from 'prop-types'
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Button from '../Button/Button';
 import ResumePDF from './ResumePDF/ResumePDF';
 import { pdf } from '@react-pdf/renderer';
-import downloadIcon from '../../assets/icons/download.svg'
+import downloadIcon from '../../assets/icons/download.svg';
 import "./ResumeOutput.css";
 
 function ResumeOutput({personalDetails, educationDetails, workExpDetails, skills}) {
+    const [scale, setScale] = useState(1);
+
+    useEffect(() => {
+        // Handler to call on window resize
+        function onResize() {
+            let docWidth = document.documentElement.clientWidth;
+            if (docWidth > 650) {
+                setScale(1);
+                return;
+            }
+    
+            setScale(docWidth / 650);
+        }
+
+        // Add event listener
+        window.addEventListener("resize", onResize);
+        // Call handler right away so state gets updated with initial window size
+        onResize();
+    })
 
     function getResume(format="") {
         return (<ResumePDF format={format} personalDetails={personalDetails} educationDetails={educationDetails} workExpDetails={workExpDetails} skills={skills} />);
@@ -31,8 +51,8 @@ function ResumeOutput({personalDetails, educationDetails, workExpDetails, skills
     }
 
     return (
-        <div className='resume-output'>
-            <div className='resume'>
+        <div className='resume-output' style={{width: 595 * scale + "px"}}>
+            <div className='resume' style={{transform: " scale(" + scale + ")"}}>
                 {getResume()}
             </div>
             <Button classes="download-btn" text="Download" icon={downloadIcon} on_click={downloadPDF} />
